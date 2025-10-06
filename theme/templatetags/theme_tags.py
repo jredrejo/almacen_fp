002329e553703e025c156fd7c7b98c978a7231ae
José Redrejo <jose@learningequality.org>
2025-10-06 20:13:24 +0200

    actualizado gitignore

--- theme/templatetags/theme_tags.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-
-@register.simple_tag
-def tailwind_css():
-    # In dev, django-tailwind injects; in prod, we fall back to compiled file
-    return """<link rel="stylesheet" href="/static/css/tailwind.css">"""
