1018f87227c8947064b347648b32ae38cd084f86
José L. Redrejo Rodríguez <jredrejo@gmail.com>
2025-11-19 20:03:17 +0100

    cambios estéticos

--- core/urls.py
+++ core/urls.py
@@ -15,23 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("almacen/", include("almacen.urls")),
-]
-
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.urls import include, path
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
+    path(
+        "password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"
+    ),
     path("", RedirectView.as_view(pattern_name="almacen:dashboard", permanent=False)),
     path("almacen/", include("almacen.urls", namespace="almacen")),
 ]
